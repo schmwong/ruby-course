b246f099c606a9daeac26a524d66644409b05d38
schmwong <smwong87@gmail.com>
2022-08-12 13:58:36 +0800

    added `downcase` and `elsif` methods

--- montys-color.rb
+++ montys-color.rb
@@ -12,8 +12,16 @@ color = gets.chop
 
 # Lesson 5: Adding an Else Statement
 
-if color == "blue"
+# Look into the downcase method and implement it into the project 
+# so that no matter what case the user inputs "blue" in, 
+# they'll see the expected response.
+
+# Look into Logical Operators to make the program respond with their favorite color being either "blue" or "green."
+if color.downcase == "blue" || "green"
     puts "Wow! My favorite color is #{color} also!"
+# Look into Elsif Statements to create a third branching pathway.
+elsif color.downcase == "red"
+    puts "It's not as good as blue, but #{color} isn't too bad."
 else
     puts "I'm not a big fan of #{color}."
 end
